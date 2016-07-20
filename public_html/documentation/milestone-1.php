284c35299ecca51469895a7ddc3b3e230f15b2de
Monica Alvarez <mmalvar13@gmail.com>
2016-07-20 11:38:22 -0600

    Entered Purpose, Audience, and Goal, Persona, Use Case

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8"/>
+		<title>Milestone One</title>
+	</head>
+	<body>
+		<h1>PWP Milestone One</h1>
+		<h2>Purpose, Audience, and Goal</h2>
+			<ol>
+				<li><strong>Purpose:</strong> This project is a professional portfolio and personal website</li>
+				<li><strong>Audience:</strong> This website is intended for a professional audience, including potential employers, professional peers, as well as clients for freelance projects</li>
+				<li><strong>Goal:</strong> The goal of this site is to present myself as a web developer and display my work for potential employers to see in order to acquire full-time or part-time employment. A secondary goal is to provide a gallery of work for potential freelance clients, to present myself as someone they would want to work with, and to give them a method of contacting me.</li>
+			</ol>
+		<h3>Persona</h3>
+			<p>
+				Name: Kiana Beckman
+				Age: 47
+				Occupation: Hiring manager at web development company
+				Technology: assigned work computer. Desktop or laptop with either WIndows or Mac OS. This device is used at work only.
+				Behaviors and attitudes: Kiana is a very involved manager, and she takes the hiring process very seriously. She wants to make sure that anyone she hires is a good fit for the company, but also that the job is a good fit for the individual and what they are looking for. It takes her no more than 30 seconds to decide if she wants to thoroughly read an applicant's resume. She is looking to fill positions for web developers, so she expects any serious applicants to have a good web presence and website. When checking out a website, it takes her no more than 10 seconds to decide if she is impressed with the website design. She will spend about 3 minutes browsing through a website; more if she is finding what she is looking for.
+				Frustrations and Needs:Kiana needs to hire an entry level junior programmer to her company. She would like to hire someone who is a good fit with the company culture, has demonstrated talent and commitment to continual learning, and who is looking to grow within the company. She wants someone who has a basic set of skills, and in working with more seasoned web developers, can get up to pace within three months on the job.
+				Goals: Kiana’s goal is to identify applicants that she would like to interview and potentially hire. She doesn’t like to invite just anyone to an interview, and wants to make sure the interview will be worth her time.
+			</p>
+	</body>
+
+</html>
