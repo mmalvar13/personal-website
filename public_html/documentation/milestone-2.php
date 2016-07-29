98765d390e81c1b7e809e66967c1086fbe565ce7
Monica Alvarez <mmalvar13@gmail.com>
2016-07-28 23:05:16 -0600

    added milestone-2 file, included site map, content strategy, and svg wireframes

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8"/>
+		<title>Milestone 2a</title>
+	</head>
+	<body>
+		<h1>Site Map</h1>
+		<p>One home page with the "about me". Home page will have navigation to a contact page and a portfolio page.</p>
+		<ul>
+			<li>Home Page/About Me</li>
+			<li>Portfolio</li>
+			<li>Contact Me</li>
+		</ul>
+		<h1>Content Strategy</h1>
+		<ul>
+			<li><strong>Home Page/About Me:</strong>Full size image header that takes up about half of the screen. Has my name and "web developer, ABQ" in the center of the image. A photo of me and a friendly about me paragraph that includes who i am, what i do, and what we can do together.</li><br>
+			<li><strong>Portfolio:</strong>Layout 2. Will include blurb about my work. Include examples of past work, mainly from DDCB. Gallery with slide show that describes images and projects</li><br>
+			<li><strong>Contact Me:</strong>Layout 2. Will include blurb about why they should contact me. Easy to read contact form. Include email address as well as links to professional social profiles like linkedin and github. </li>
+		</ul>
+
+		<h1>WireFrames:Mobile Layouts</h1>
+		<img src="../../images/mobileLayout1.svg"/>
+		<h1>WireFrames:Web Layouts</h1>
+		<h2>Home Page</h2>
+		<img src="../../images/webLayout1.svg"/>
+		<h2>Content Page</h2>
+		<img src="../../images/webLayout2.svg"/>
+
+	</body>
+</html>
