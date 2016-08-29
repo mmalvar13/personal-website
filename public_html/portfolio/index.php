1319a62b26ec5f487cf4e5b2c09eaa58155254de
Monica Alvarez <mmalvar13@gmail.com>
2016-08-29 11:17:11 -0600

    added carousel comments

--- public_html/portfolio/index.php
+++ public_html/portfolio/index.php
@@ -85,6 +85,55 @@
 				</div>
 
 			</header>
+			<main>
+				<div class="container">
+					<div class="row">
+						<div class="col-md-7">
+
+							<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+								<!-- Indicators -->
+								<ol class="carousel-indicators">
+									<li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+									<li data-target="#carousel-example-generic" data-slide-to="1"></li>
+									<li data-target="#carousel-example-generic" data-slide-to="2"></li>
+								</ol>
+
+								<!-- Wrapper for slides -->
+								<div class="carousel-inner" role="listbox">
+									<div class="item active">
+										<img src="../images/slideshow1.png" alt="slideshow example">
+										<div class="carousel-caption">
+											<p>This is an example of a description of my project</p>
+											<a href="https://www.facebook.com">Facebook Link to Practice</a>
+										</div>
+									</div>
+									<div class="item">
+										<img src="../images/slideshow2.png" alt="slideshow example">
+										<div class="carousel-caption">
+											<p>This project was made for reasons, check it out at the link below</p>
+											<a href="https://www.github.com">I'm the Link Below</a>
+										</div>
+									</div>
+									...
+								</div>
+
+								<!-- Controls -->
+								<a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+									<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+									<span class="sr-only">Previous</span>
+								</a>
+								<a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+									<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+									<span class="sr-only">Next</span>
+								</a>
+							</div>
+
+
+
+						</div>
+					</div>
+				</div>
+			</main>
 
 		</div>
 
